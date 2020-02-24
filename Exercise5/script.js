1f28037bbf5689cc6037c8b5110156ba4f69f434
Deniable Plausibility <59665674+gabpaulson@users.noreply.github.com>
2020-02-24 10:56:02 -0800

    Add files via upload

---
+++ Exercise5/script.js
@@ -0,0 +1,28 @@
+var beverage1 = prompt("This Madlibs is in celebration of Mug Day (whichever day of November is rainiest), a Muglibs. Enter a beverage:");
+beverage1 = beverage1.toLowerCase();
+
+var noun1 = prompt("Enter a noun:");
+var noun1u = noun1.toUpperCase();
+noun1 = noun1.toLowerCase();
+
+
+var quality1 = prompt("Enter a quality:");
+quality1 = quality1.toLowerCase();
+
+var noun2 = prompt("Enter a noun:");
+noun2 = noun2.toLowerCase();
+
+var noun3 = prompt("Enter a noun:");
+noun3 = noun3.toLowerCase();
+
+var adj1 = prompt("Enter an adjective:");
+adj1 = adj1.toLowerCase();
+
+var beverage2 = prompt("Enter a beverage:");
+beverage2 = beverage2.toLowerCase();
+
+var beverage3 = prompt("Enter a beverage:");
+beverage3 = beverage3.toLowerCase();
+
+
+document.write("\"Lastly, "+beverage1+"--unless one is drinking it in the Russian style--should be drunk WITHOUT "+noun1u+". I know very well that I am in a minority here. But still, how can you call yourself a true "+beverage1+"-lover if you destroy the "+quality1+" of your "+beverage1+" by putting "+noun1+" in it? It would be equally reasonable to put in "+noun2+" or "+noun3+". Really, "+beverage1+" is meant to be "+adj1+", just as "+beverage2+" is meant to be "+adj1+". If you sweeten it, you are no longer tasting the "+beverage1+", you are merely tasting the "+noun1+"; you could make a very similar drink by dissolving "+noun1+" in plain hot "+beverage3+".\" ― George Orwell, A nice cup of "+beverage1)
